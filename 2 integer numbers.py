c8da68f9da1b7965dc7b4025458fa319d096bab7
neetiv <67287846+neetiv@users.noreply.github.com>
2020-07-21 15:21:30 -0700

    gcd and factors update
    updated factors code to be compatible with logical calculation of gcd

--- 2 integer numbers.py
+++ 2 integer numbers.py
@@ -1,19 +1,32 @@
 inp = input("Give two numbers(seperated by commas):  ")
 nums = inp.split(',')
 #print(nums)
-lst = []
-x = 0
+one = []
+two = []
 
-for j in range(0, len(nums)):
-    for i in range(1, int(nums[x])+1):
-        if int(nums[x]) % i == 0:
-            lst.append(i)
-    print("The factors of",nums[x],"are:",lst)
-    x = x+1
-    lst.clear()
 
-import math
-gcd=(math.gcd(int(nums[0]),int(nums[1])))
-print("The GCD of",nums[0],"and",nums[1],"is",gcd)
+for y in range(1, int(nums[0])):
+    if int(nums[0]) % y == 0:
+        one.append(y)
+print("The factors of",nums[0],"are:",one)
 
-print("The LCM of",nums[0],"and",nums[1],"is",int((int(nums[0])*int(nums[1]))/gcd))
+
+for a in range(1, int(nums[1])):
+    if int(nums[1]) % a == 0:
+        two.append(a)
+print("The factors of",nums[1],"are:",two)
+
+
+##gcd = 1
+##for i in range(0, len(one)):
+##    for j in range(0, len(two)):
+##        if one[-1*i] == two[-1*j]:
+##            gcd=(one[-1*i])
+##            print(gcd, "is the GCD of", nums[0], "and", nums[1])
+##            break
+##
+####import math
+####gcd=(math.gcd(int(nums[0]),int(nums[1])))
+####print("The GCD of",nums[0],"and",nums[1],"is",gcd)
+##
+##print("The LCM of",nums[0],"and",nums[1],"is",int((int(nums[0])*int(nums[1]))/gcd))
