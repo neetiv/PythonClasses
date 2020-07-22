79ab76f78fe4b54ececb4acb4e3b663c04425167
neetiv <67287846+neetiv@users.noreply.github.com>
2020-07-22 15:01:38 -0700

    started the project

---
+++ Prime Factorization.py
@@ -0,0 +1,9 @@
+num = int(input("Enter a number:  "))
+store = num
+factors = []
+for g in range(1,(x)+1):
+    if int(x) % g == 0:
+        if 
+        factors.append(g)
+
+print("The prime factorization of",store,"is",factors)
