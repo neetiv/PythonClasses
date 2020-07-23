c641fda104ce3b250cf60bac44312e496f454c77
neetiv <67287846+neetiv@users.noreply.github.com>
2020-07-23 14:02:34 -0700

    fixed basic code
    working on print formatting

--- Prime Factorization.py
+++ Prime Factorization.py
@@ -1,9 +1,14 @@
 num = int(input("Enter a number:  "))
 store = num
 factors = []
-for g in range(1,(x)+1):
-    if int(x) % g == 0:
-        if 
+
+for g in range(2,(num)+1):
+    while (int(num) % g == 0):
         factors.append(g)
+        factors.append(' ')
+        num = num/g
+for x in range(1, len(factors)):
+    if len(factors) % x != 0:
+        factors[x] = 
 
 print("The prime factorization of",store,"is",factors)
