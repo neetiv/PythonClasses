4a6670dc0b692f03e22a61a6bd25fcce676b8974
neetiv <67287846+neetiv@users.noreply.github.com>
2020-08-07 14:03:56 -0700

    hi

--- guessing game(computer guesses).py
+++ guessing game(computer guesses).py
@@ -1,35 +1,28 @@
 print('''Choose any integer between 1 and 100 and keep it in your mind. The computer will guess a number.
 ''')
-lst=[]
-small = 0
-large = 0
 
-for i in range (1,101):
-    lst.append(i)
-#print(lst)
+small = 1
+large = 100
+
 
 while True:
-    print(len(lst))
-    guess = lst[int(len(lst)/2)]
+    guess = int((small+large)/2)
     
     print("I guess", guess)
     ans = input('''
 Tell the computer...
-A: Your number is higher
-B: Your number is lower
+A: The number in your mind is higher
+B: Your number in your mind is lower
 C: The computer guessed right!
     
 ''')
 
     if ans == 'A' or ans == 'a':
         small = guess+1
-        large = 
 
     elif ans == 'B' or ans == 'b':
-        del lst[guess: lst[-2]]
+        large = guess-1
 
     elif ans == 'C' or ans == 'c':
-        print(":)")
+        print("Your number was", guess, ":)")
         break
-
-    
--- guessing game.py
+++ guessing game.py
@@ -1,18 +1,46 @@
 import random
-num = random.randint(1, 100)
+level = input('''
+Level Selection:
+1: Easy
+2: Medium
+3: Hard
+
+''')
+
+maximum = 0
+guesses = 0
+if level == '1':
+    maximum = 100
+    guesses = 8
+elif level == '2':
+    maximum = 1000
+    guesses = 20
+elif level == '3':
+    maximum = 10000
+    guesses = 26
+
+num = random.randint(1, maximum)
 guess = 0
 while True:
-    i = int(input('''Guess a number between 1 and 100:  '''))
+    i = int(input('''
+Guess a number between 1 and %s:  ''' % maximum))
+    x = abs(num-i)
+    
     if i == num:
         print('You guessed right!')
-        guess+=1
         break
-    elif i > num:
-        print('Try lower')
-        guess+=1
-    elif i < num:
-        print('Try higher')
-        guess+=1
-    if guess==6:
+    elif x <= 5:
+        print("Very Hot")
+    elif x <= 10:
+        print("Hot")
+    elif x <= 20:
+        print("Warm")
+    elif x <= 50:
+        print("Cold")
+    else:
+        print("Very Cold")
+        
+    guess+=1
+    if guess==guesses:
         print("You are out of guesses. The number was:  ", num)
         break
